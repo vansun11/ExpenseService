647c112c46035ea8d31209a1871a6639374234b6
Qiu Wan <QWan@slb.com>
2022-10-26 13:59:12 +0800

    feat: add test for 'Should_throw_unexpected_project_exception_if_project_is_invalid'

--- ExpenseServiceTest/ExpenseServiceTest.cs
+++ ExpenseServiceTest/ExpenseServiceTest.cs
@@ -1,4 +1,5 @@
 using Expense.Service;
+using Expense.Service.Exceptions;
 using Expense.Service.Expense;
 using Expense.Service.Projects;
 using Xunit;
@@ -57,8 +58,11 @@ namespace Expense.Service.Test
         public void Should_throw_unexpected_project_exception_if_project_is_invalid()
         {
             // given
+            Project project = new (ProjectType.UNEXPECTED_PROJECT_TYPE, "Project Unexpected");
             // when
             // then
+            Assert.Throws<UnexpectedProjectTypeException>(
+                () => ExpenseService.GetExpenseCodeByProjectTypeAndName(project));
         }
     }
 }
